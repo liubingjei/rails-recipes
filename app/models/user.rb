f9e3f9d9988b4ff38f3cdcb18f33ca01927a0608
liubingjie <breeder1121@hotmail.com>
2017-08-16 22:50:57 +0800

    创建用户资料管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+
+  has_one :profile
+  accepts_nested_attributes_for :profile
+
   has_many :memberships
   has_many :groups, :through => :memberships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 
   def display_name
     self.email.split("@").first
